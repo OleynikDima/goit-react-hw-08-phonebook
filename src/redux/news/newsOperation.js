0007713754df7fa4b5542bb75f86d4a92af85eb0
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-09-15 15:04:06 +0300

    fix get news

--- src/redux/news/newsOperation.js
+++ src/redux/news/newsOperation.js
@@ -6,7 +6,7 @@ const fetchNewsArr = () => dispatch => {
 dispatch(newsAction.newsFetchRequest());
 
 axios
-    .get('http://newsapi.org/v2/top-headlines?country=us&apiKey=bcaafe07bfdc4ea5885fe47775020d18')
+    .get('https://newsapi.org/v2/top-headlines?country=us&apiKey=bcaafe07bfdc4ea5885fe47775020d18')
             .then(response => dispatch(newsAction.newsFetchSuccess(response.data.articles)))
             .catch(error => dispatch(newsAction.fetchItemError(error)))
 }

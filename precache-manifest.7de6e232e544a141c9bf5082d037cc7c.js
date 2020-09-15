1be2c0dc20ae417a522b9bc952c4775556580cb2
OleynikDima <57680550+OleynikDima@users.noreply.github.com>
2020-09-15 14:46:06 +0300

    Updates

---
+++ precache-manifest.7de6e232e544a141c9bf5082d037cc7c.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c6b26dea1ee59ff48180c0680cf1ff3",
+    "url": "/goit-react-hw-07-phonebook/index.html"
+  },
+  {
+    "revision": "a8515d30cd559b286487",
+    "url": "/goit-react-hw-07-phonebook/static/css/3.31adb57a.chunk.css"
+  },
+  {
+    "revision": "f45e5ec7ec71380314c1",
+    "url": "/goit-react-hw-07-phonebook/static/css/4.3775690f.chunk.css"
+  },
+  {
+    "revision": "39312c9aab32669b6b5f",
+    "url": "/goit-react-hw-07-phonebook/static/css/6.e71c2cbc.chunk.css"
+  },
+  {
+    "revision": "252a8feb965ad7f671e9",
+    "url": "/goit-react-hw-07-phonebook/static/css/7.19dbd706.chunk.css"
+  },
+  {
+    "revision": "5e78dca9a3f718d6e74b",
+    "url": "/goit-react-hw-07-phonebook/static/css/main.4f9ac4a1.chunk.css"
+  },
+  {
+    "revision": "f62535fab654f95a11d5",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.673494c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-07-phonebook/static/js/2.673494c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8515d30cd559b286487",
+    "url": "/goit-react-hw-07-phonebook/static/js/3.b87b94fa.chunk.js"
+  },
+  {
+    "revision": "f45e5ec7ec71380314c1",
+    "url": "/goit-react-hw-07-phonebook/static/js/4.6c73b0c8.chunk.js"
+  },
+  {
+    "revision": "f16e1b044221da79f56b",
+    "url": "/goit-react-hw-07-phonebook/static/js/5.b73857ab.chunk.js"
+  },
+  {
+    "revision": "3d2d88b7877918a326634499feeab80c",
+    "url": "/goit-react-hw-07-phonebook/static/js/5.b73857ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39312c9aab32669b6b5f",
+    "url": "/goit-react-hw-07-phonebook/static/js/6.5334a334.chunk.js"
+  },
+  {
+    "revision": "252a8feb965ad7f671e9",
+    "url": "/goit-react-hw-07-phonebook/static/js/7.932f98dc.chunk.js"
+  },
+  {
+    "revision": "5e78dca9a3f718d6e74b",
+    "url": "/goit-react-hw-07-phonebook/static/js/main.d2183ad3.chunk.js"
+  },
+  {
+    "revision": "bc7c1208d89e5080405b",
+    "url": "/goit-react-hw-07-phonebook/static/js/runtime-main.18d18701.js"
+  },
+  {
+    "revision": "2efba7d2021ff9cc0ebb277417d18b03",
+    "url": "/goit-react-hw-07-phonebook/static/media/david-jusko-y91SN93DBiM-unsplash.2efba7d2.jpg"
+  },
+  {
+    "revision": "6efb8305e609228e84e9910fea042b44",
+    "url": "/goit-react-hw-07-phonebook/static/media/kisspng-user-logo-information-service-design-5ba34f88e63387.4679724515374293849429.6efb8305.png"
+  },
+  {
+    "revision": "7ea4af7d8401d2b43ee841bfa2abe89d",
+    "url": "/goit-react-hw-07-phonebook/static/media/pngegg.7ea4af7d.png"
+  }
+]);
